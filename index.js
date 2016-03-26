23ff77db6e1371b526182dde2feaaf5636087d3b
FG Ribreau <github@fgribreau.com>
2016-03-26 14:00:41 +0100

    feat(sendgrid)

---
+++ index.js
@@ -0,0 +1,93 @@
+var Sendgrid = require('sendgrid');
+var _ = require('lodash');
+var deepExtend = require('deep-extend');
+
+function MailProvider(apiKey, options) {
+  if (!apiKey) {
+    throw new Error("require('transacemail-sendgrid')('apikey'), api key is required.");
+  }
+
+  this.options = deepExtend({}, MailProvider.DEFAULT_OPTION, options);
+  this.sendgrid = Sendgrid(apiKey);
+}
+
+/**
+ * Debug mode
+ * @type {Boolean}
+ */
+MailProvider.DEBUG_MODE = false;
+
+/**
+ * Default options
+ * @type {Object}
+ */
+MailProvider.DEFAULT_OPTION = {
+  from: 'Transacemail',
+  subject: "node-transacemail-sendgrid"
+};
+
+/**
+ * Send method
+ * @param  {Object}   mail TransacEmail Mail object
+ * @param  {Function} fn   Callback to call when done
+ */
+MailProvider.prototype.send = function(mail, fn) {
+  if (!mail.data || !mail.data.email) {
+    throw new Error("The `email` field is required");
+  }
+
+  var options = deepExtend({}, this.options, mail.sendgrid, {
+    /**
+     * HTML content
+     * @type {String}
+     */
+    html: mail.html,
+
+    /**
+     * Text content
+     * @type {String}
+     */
+    text: mail.text,
+
+    /**
+     * To email
+     * @type {String}
+     */
+    to: mail.data.email,
+
+    /**
+     * To user name
+     * @type {String}
+     */
+    toname: mail.data.name
+  });
+
+  if (MailProvider.DEBUG_MODE) {
+    console.log("Sending an email to " + mail.data.email);
+    console.log(JSON.stringify(options, null, 2));
+    fn(null, [{
+      email: mail.data.email,
+      status: 'sent'
+    }]);
+    return;
+  }
+
+  this.sendgrid.send(options, function(error, response) {
+    //everything's good, lets see what sendgrid said
+    //[ { email: 'plop@fgribreau.com', status: 'sent' } ]
+    fn(error ? JSON.stringify(error) : null, response);
+  });
+};
+
+/**
+ * TransacEmail-Mandrill factory
+ * @param  {String} apiKey        Mandrill API Key
+ * @param  {Object} defaultOption Default send option
+ * @return {MailProvider}
+ */
+function Factory(apiKey, defaultOption) {
+  return new MailProvider(apiKey, defaultOption || {});
+}
+
+Factory.MailProvider = MailProvider;
+module.exports = Factory;
---
+++ index.test.js
@@ -0,0 +1,22 @@
+const t = require('chai').assert;
+const assert = require('assert');
+
+describe('sendgrid', function(){
+  it('should send an email', function(f){
+    var SENDGRID_KEY = process.env.SENDGRID_KEY;
+    assert(SENDGRID_KEY, 'SENDGRID_KEY environment variable is missing');
+    var provider = require('.')(SENDGRID_KEY);
+    provider.send({
+      html: '<strong>hello :)</strong>',
+      text: 'hello :)',
+      data:{
+        email: 'transacemail-sendgrid@fgribreau.com',
+        name: 'Transacemail Sendgrid'
+      }
+    }, function(err, result){
+      t.strictEqual(err, null);
+      t.deepEqual(result, {"message":"success"});
+      f();
+    });
+  });
+});
